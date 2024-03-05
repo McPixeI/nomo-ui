52524d168c3b650724e35ab19713672d09d922dc
Samuel Torres <torres.88.bcn@gmail.com>
2024-03-05 10:56:14 +0100

    lint test

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,8 +6,13 @@ import { buttonStyles } from './styles';
 
 type ButtonProps = ComponentProps<'button'> & VariantProps<typeof buttonStyles>;
 
+
+
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, variant, size, colorscheme, className }, ref) => {
+
+    console.log('first'); 
+
     return (
       <button
         ref={ref}
